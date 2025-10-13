697d922ae893ca2c3bbff6d9425bee0b947bb91f
PolpOnline <git@t0mmy.anonaddy.com>
2025-10-14 01:50:30 +0200

    Fix add absence

--- src/backend/src/web/endpoints/protected/absence/post.rs
+++ src/backend/src/web/endpoints/protected/absence/post.rs
@@ -2,7 +2,7 @@ use crate::app::openapi::DASHBOARD_TAG;
 use crate::users::AuthSession;
 use axum::response::IntoResponse;
 use axum_serde::Sonic;
-use chrono::NaiveDateTime;
+use chrono::{NaiveDate, NaiveTime};
 use http::StatusCode;
 use serde::Deserialize;
 use tracing::error;
@@ -10,9 +10,11 @@ use utoipa::ToSchema;
 
 #[derive(Debug, Deserialize, ToSchema)]
 pub struct AddAbsenceRequest {
+    /// The date of the absence. If not provided, defaults to today.
+    date: Option<NaiveDate>,
     absent_teacher_id: i32,
-    begin_ts: NaiveDateTime,
-    _end_ts: NaiveDateTime,
+    begin_time: NaiveTime,
+    end_time: NaiveTime,
 }
 
 #[utoipa::path(
@@ -36,25 +38,30 @@ pub async fn post(
         return (StatusCode::UNAUTHORIZED, "Unauthorized").into_response();
     };
 
-    if req.begin_ts > req._end_ts {
+    if req.begin_time > req.end_time {
         return (StatusCode::BAD_REQUEST, "begin_ts must be before end_ts").into_response();
     }
 
     let res = sqlx::query!(
         r#"
-        INSERT INTO absence (absent_teacher_lesson, absence_date)
-        SELECT $1, COALESCE($2::timestamp, CURRENT_DATE)::date
-        WHERE EXISTS (
-            SELECT 1
-            FROM lesson le
-            JOIN teacher t ON le.teacher_id = t.id
-            JOIN import i ON t.import_id = i.id
-            WHERE le.id = $1 AND i.user_id = $3
+        WITH lessons AS (
+          SELECT le.id
+          FROM lesson le
+          JOIN teacher t ON le.teacher_id = t.id
+          JOIN import i ON t.import_id = i.id
+          WHERE le.teacher_id = $1
+            AND le.day = EXTRACT(DOW FROM COALESCE($2, CURRENT_DATE)::date)::int
+            AND le.time::time BETWEEN ($3::time) AND ($4::time)
+            AND i.user_id = $5
         )
+        INSERT INTO absence (absent_teacher_lesson, absence_date)
+        SELECT l.id, ($2::date)
+        FROM lessons l;
         "#,
         req.absent_teacher_id,
-        // TODO: Use end_ts to create multiple absences
-        req.begin_ts,
+        req.date,
+        req.begin_time,
+        req.end_time,
         user.id
     )
     .execute(&auth_session.backend.db)
