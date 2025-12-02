2ec19d67143a9145277321064b79b9f2597f746a
PolpOnline <git@t0mmy.anonaddy.com>
2025-12-02 23:56:19 +0100

    More spacing across items

--- src/frontend/src/lib/components/AbsenceCard.svelte
+++ src/frontend/src/lib/components/AbsenceCard.svelte
@@ -1,41 +1,44 @@
 <script lang="ts">
-	import { Badge } from '$components/ui/badge';
-	import type { components } from '$lib/api/schema';
-	import LucideDoorClosed from '~icons/lucide/door-closed';
-	import LucideUsers from '~icons/lucide/users';
+    import {Badge} from '$components/ui/badge';
+    import type {components} from '$lib/api/schema';
+    import LucideDoorClosed from '~icons/lucide/door-closed';
+    import LucideUsers from '~icons/lucide/users';
 
-	let { absence }: { absence: components['schemas']['Absence'] } = $props();
+    let {absence}: { absence: components['schemas']['Absence'] } = $props();
 </script>
 
 <div class="w-full rounded-xl border p-4">
-	<div class="mb-0.5 ml-1 text-lg font-bold tracking-wide">
-		{absence.absent_teacher}
-	</div>
+    <div class="mb-3 ml-1 text-lg font-bold tracking-wide">
+        {absence.absent_teacher}
+    </div>
 
-	{#each absence.classes as hour (hour.id)}
-		{@const formattedTime = hour.time.split(':', 2).slice(0, 2).join(':')}
+    <div class="flex space-y-2 flex-col">
+        {#each absence.classes as hour (hour.id)}
+            {@const formattedTime = hour.time.split(':', 2).slice(0, 2).join(':')}
+            <div>
+                <div class="ml-2 text-lg font-medium text-muted-foreground">{formattedTime}</div>
 
-		<div class="ml-2 text-lg font-medium text-muted-foreground">{formattedTime}</div>
+                <div class="rounded-xl border p-3 shadow-sm">
+                    <div class="flex flex-col">
+                        {#if hour.absent_status === 'uncovered'}
+                            <div class="text-sm font-semibold text-destructive">SCOPERTO</div>
+                        {:else}
+                            <div class="text-sm">{hour.substitute_teacher}</div>
+                        {/if}
 
-		<div class="rounded-xl border p-3 shadow-sm">
-			<div class="flex flex-col">
-				{#if hour.absent_status === 'uncovered'}
-					<div class="text-sm font-semibold text-destructive">SCOPERTO</div>
-				{:else}
-					<div class="text-sm">{hour.substitute_teacher}</div>
-				{/if}
-
-				<div class="mt-2 flex gap-2">
-					<Badge variant="secondary">
-						<LucideDoorClosed class="mr-1 size-4" />
-						{hour.group}
-					</Badge>
-					<Badge variant="secondary">
-						<LucideUsers class="mr-1 size-4" />
-						{hour.room}
-					</Badge>
-				</div>
-			</div>
-		</div>
-	{/each}
+                        <div class="mt-2 flex gap-2">
+                            <Badge variant="secondary">
+                                <LucideDoorClosed class="mr-1 size-4"/>
+                                {hour.group}
+                            </Badge>
+                            <Badge variant="secondary">
+                                <LucideUsers class="mr-1 size-4"/>
+                                {hour.room}
+                            </Badge>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        {/each}
+    </div>
 </div>
